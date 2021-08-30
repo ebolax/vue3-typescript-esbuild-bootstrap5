c96a4afa41b1ad43100a7a723fdb1b4fb67cf8e1
ebolax <ebolax@gmail.com>
2021-08-30 21:27:56 +0300

    eslint

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,12 @@
+/* global process */
+
 import esbuild from "esbuild";
 import vuePlugin from "esbuild-plugin-vue3";
 
 const args = process.argv.slice(2);
 
-let watch = args[0] === "dev";
-let minify = args[0] === "prod";
+const watch = args[0] === "dev";
+const minify = args[0] === "prod";
 
 esbuild.build({
     entryPoints: ["src/main.ts"],
